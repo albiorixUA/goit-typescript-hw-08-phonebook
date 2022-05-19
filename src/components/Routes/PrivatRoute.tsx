f860e9bea105e5ba556c6376376ec6836acd4216
unknown <steblynamaxim@gmail.com>
2022-05-19 12:19:38 +0300

    fix privat route

--- src/components/Routes/PrivatRoute.tsx
+++ src/components/Routes/PrivatRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from 'redux/authSlice';
 import { RoutesPropsType } from 'types/componentTypes';
 
-const PrivateRoute = ({ redirectTo = '/', children }: RoutesPropsType) => {
+const PrivateRoute = ({ redirectTo = '/login', children }: RoutesPropsType) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return isLoggedIn ? children : <Navigate to={redirectTo} />;
 };
