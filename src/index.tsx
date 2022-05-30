ac43533d98fb099440891e08ff309eda6870e2d4
Maxim Steblyna <86981447+albiorixUA@users.noreply.github.com>
2022-05-30 18:44:00 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+            <BrowserRouter basename="/goit-typescript-hw-08-phonebook/">
             <App />
           </BrowserRouter>
         </PersistGate>
